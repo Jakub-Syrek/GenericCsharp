bbaec4ff90674a675e4636076dec14b89db52436
Jakub Syrek Mac <jakubvonsyrek@gmail.com>
2018-11-24 17:25:00 +0100

    Update Program.cs

--- GenericCsharp/Program.cs
+++ GenericCsharp/Program.cs
@@ -2,6 +2,15 @@
 
 namespace GenericCsharp
 {
+    class Point
+    {
+        public int X { get; set; } = 1;
+        public int Y { get; set; } = 1;
+        public int CompareTo(Point other)
+        {
+            return (X * X + Y * Y).CompareTo(other.X * other.X + other.Y * other.Y);
+        }
+    }
     class MainClass
     {
         public static void Main(string[] args)
@@ -9,16 +18,16 @@ namespace GenericCsharp
             Console.WriteLine(Max<double>(3.0, 5.0));
             Console.WriteLine(Max<int>(3, 5));
             Console.WriteLine(Max<decimal>(3.0m, (decimal)5.5));
-
+            Console.WriteLine(Max<Point>(new Point(), new Point()));
         }
         static double Max1(double x, double y)
             {
             if (x < y) { return y; } else { return x; }
             }
             //Generic
-            static T Max<T>(T x, T y) where T : IComparable<T>
+        static T Max<T>(T x, T y , Comparison<T> funkcjaPorownujaca) //where T : IComparable<T>
             {
-            if (x.CompareTo(y) < 0) return y;
+            if (funkcjaPorownujaca.Compare(x, y) < 0) return y;           //(x.CompareTo(y) < 0) return y;
             else return x;
             }
 
